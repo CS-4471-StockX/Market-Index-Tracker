448ea8a2612b5eb323d404f4a606bda444b68c3e
wanger26 <jwanger@uwo.ca>
2021-10-27 15:53:27 -0400

    Inital upload

---
+++ src/main/java/com/stockx/marketindextracker/MarketIndexTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.stockx.marketindextracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketIndexTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketIndexTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockx/marketindextracker/MarketIndexTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockx.marketindextracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketIndexTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
