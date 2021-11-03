358e6c7f7eff290e9c5bb443cbf898012e688497
Evan Rome-Bailey <eromebai@uwo.ca>
2021-11-02 21:54:34 -0400

    Error: Cannot deserialize value of type `com.stockx.marketindextracker.MarketIndexWrap` from Array value (token `JsonToken.START_ARRAY`)
    Not functional

--- src/main/java/com/stockx/marketindextracker/controllers/MarketIndexTrackerController.java
+++ src/main/java/com/stockx/marketindextracker/controllers/MarketIndexTrackerController.java
@@ -1,6 +1,8 @@
 package com.stockx.marketindextracker.controllers;
 
 import com.stockx.marketindextracker.HistoricalMarketIndexPrice;
+import com.stockx.marketindextracker.MarketIndex;
+import com.stockx.marketindextracker.MarketIndexWrap;
 import com.stockx.marketindextracker.services.MarketIndexTrackerServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,5 +18,10 @@ public class MarketIndexTrackerController {
     @GetMapping("/historical/market-index-price")
     public HistoricalMarketIndexPrice getHistoricalMarketIndexPriceByTicker(@RequestParam("ticker") String ticker) {
         return marketIndexTrackerServices.getHistoricalMarketIndexPriceByTicker(ticker);
+    }
+
+    @GetMapping("/market-index")
+    public MarketIndexWrap getMarketIndexByTicker(@RequestParam("ticker") String ticker){
+        return marketIndexTrackerServices.getMarkerIndexByTicker(ticker);
     }
 }
--- src/main/java/com/stockx/marketindextracker/services/MarketIndexTrackerServices.java
+++ src/main/java/com/stockx/marketindextracker/services/MarketIndexTrackerServices.java
@@ -1,6 +1,8 @@
 package com.stockx.marketindextracker.services;
 
 import com.stockx.marketindextracker.HistoricalMarketIndexPrice;
+import com.stockx.marketindextracker.MarketIndex;
+import com.stockx.marketindextracker.MarketIndexWrap;
 import com.stockx.marketindextracker.adapters.FinancialModelingPrepAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,5 +15,9 @@ public class MarketIndexTrackerServices {
 
     public HistoricalMarketIndexPrice getHistoricalMarketIndexPriceByTicker(String ticker) {
         return financialModelingPrepAdapter.getHistoricalMarketIndexPriceByTicker(ticker);
+    }
+
+    public MarketIndexWrap getMarkerIndexByTicker(String ticker){
+        return financialModelingPrepAdapter.getCurrentIndex(ticker);
     }
 }
