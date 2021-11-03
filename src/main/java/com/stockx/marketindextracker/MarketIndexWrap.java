07b8c148b5b2efffbdebc941a85743d23cbe86b5
wanger26 <jwanger@uwo.ca>
2021-11-02 22:10:23 -0400

    Fixed deserialization issue with object

--- src/main/java/com/stockx/marketindextracker/MarketIndexWrap.java
+++ src/main/java/com/stockx/marketindextracker/MarketIndexWrap.java
@@ -1,10 +1,12 @@
 package com.stockx.marketindextracker;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class MarketIndexWrap {
 
     private List<MarketIndex> Indexes;
