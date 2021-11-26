6ca4399f717b06b7427d52b95141b0741f8395ab
wanger26 <jwanger@uwo.ca>
2021-11-26 10:55:01 -0500

    Fixed CORS

--- src/main/java/com/stockx/marketindextracker/controllers/MarketIndexTrackerController.java
+++ src/main/java/com/stockx/marketindextracker/controllers/MarketIndexTrackerController.java
@@ -1,10 +1,10 @@
 package com.stockx.marketindextracker.controllers;
 
-import com.stockx.marketindextracker.HistoricalMarketIndexPrice;
 import com.stockx.marketindextracker.HistoricalMarketIndexPrices;
 import com.stockx.marketindextracker.MarketIndex;
 import com.stockx.marketindextracker.services.MarketIndexTrackerServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +15,13 @@ public class MarketIndexTrackerController {
     @Autowired
     MarketIndexTrackerServices marketIndexTrackerServices;
 
+    @CrossOrigin(origins = "https://www.stockx.software")
     @GetMapping("/historical/market-index-price")
     public HistoricalMarketIndexPrices getHistoricalMarketIndexPriceByTicker(@RequestParam("ticker") String ticker) {
         return marketIndexTrackerServices.getHistoricalMarketIndexPriceByTicker(ticker);
     }
 
+    @CrossOrigin(origins = "https://www.stockx.software")
     @GetMapping("/market-index")
     public MarketIndex getMarketIndexByTicker(@RequestParam("ticker") String ticker) {
         return marketIndexTrackerServices.getMarkerIndexByTicker(ticker);
