4c8bb15783a5d7c2b66a3f5c8b61addf8745d2ec
wanger26 <jwanger@uwo.ca>
2021-12-09 09:52:27 -0500

    Removed creds

--- src/main/java/com/stockx/marketindextracker/configurations/AppConfiguration.java
+++ src/main/java/com/stockx/marketindextracker/configurations/AppConfiguration.java
@@ -1,6 +1,7 @@
 package com.stockx.marketindextracker.configurations;
 
 import com.stockx.marketindextracker.adapters.FinancialModelingPrepAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,9 +9,14 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfiguration {
 
+    @Value("${financialModelingPrep.apiKey1}")
+    private String financialModelingPrepApiKey1;
+    @Value("${financialModelingPrep.apiKey2}")
+    private String financialModelingPrepApiKey2;
+
     @Bean
-    public FinancialModelingPrepAdapter financialModelingPrepAdapter() {
-        return new FinancialModelingPrepAdapter();
+    public FinancialModelingPrepAdapter financialModelingPrepAdapter(RestTemplate restTemplate) {
+        return new FinancialModelingPrepAdapter(restTemplate, financialModelingPrepApiKey1, financialModelingPrepApiKey2);
     }
 
     @Bean
